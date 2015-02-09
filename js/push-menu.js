18cee7ac43b33b6664e39d25756b1ab2bbbabd54
lorefranko <lorefranko@gmai.com>
2015-02-09 11:52:46 -0500

    feat(js) add jQuery

--- js/push-menu.js
+++ js/push-menu.js
@@ -1,19 +1,4 @@
-// Needs latest version of jQuery to run
-
-$(function() {
-  $('body').removeClass('noscript');
-
-  $('.toggle-btn, .close-btn').click(function() {
-    toggleNav();
-  });
+$(document).ready(function() {
+  $("#my-menu").mmenu();
 });
 
-function toggleNav() {
-  if ($('.site-wrapper').attr('data-state') == 'slide-closed') {
-    // Display Nav when closed
-    $('.site-wrapper').attr('data-state', 'slide-open');
-  } else {
-    // Hide Nav when open
-    $('.site-wrapper').attr('data-state', 'slide-closed');
-  }
-}
